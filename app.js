713d0eddbdd6da58a3679797c6a6bc530b92c225
Syed Eebad Reza <95500079+eebadreza@users.noreply.github.com>
2023-02-18 10:07:05 +0000

    if else

--- app.js
+++ app.js
@@ -42,9 +42,10 @@ app.post("/", (req, res) => {
 
   const request = https.request(url, options, (response) =>{
         
-        response.statusCode === 200
-          ? res.sendFile(__dirname + "/success.html")
-          : res.sendFile(__dirname + "/failure.html");
+        if(response.statusCode === 200)
+           res.sendFile(__dirname + "/success.html")
+        else
+           res.sendFile(__dirname + "/failure.html");
 
         
         response.on('data', (data) =>{
